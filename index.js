7492fa1b888c14c00f30d353fb50c14962c22d56
Roman Chertishchev <rchertishchev@gmail.com>
2023-03-23 14:13:48 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+(function () {window.addEventListener('message', function (e) {
+    if (e.origin != "https://experts.ai") return;
+    document.getElementById('expertsAiWidget').style.height = e.data + 'px';
+}, false);
+
+    window.addEventListener('message', function (e) {
+        if (e.origin !== "https://experts.ai") return;
+        var data = e.data;
+        if (typeof(window[data.function]) == "function") {
+            window[data.function].call();
+        }
+    }, false);
+    function scrollUp() {
+        var scrollDiv = document.getElementById("expertsWidget").offsetTop; // you may need to subtract header's offsetTop
+        window.scrollTo({top: scrollDiv, behavior: 'smooth'});
+    }
+
+    const thisPageUrl = new URL( document.location.host + document.location.pathname);
+
+    const searchParam = new URLSearchParams( document.location.search);
+
+    if (searchParam.has('widget')) {
+        const path = searchParam.get('widget');
+        const url = new URL(path);
+        url.searchParams.append('base_url', thisPageUrl.toString());
+        document.getElementById('expertsAiWidget').src = url;
+    } else {
+        const url = new URL(document.getElementById('expertsAiWidget').src);
+        url.searchParams.append('base_url', thisPageUrl.toString());
+        document.getElementById('expertsAiWidget').src =  url;
+    }
+}());
